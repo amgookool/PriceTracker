7975889389b264725983146d6f0b1965f17d0db3
Adrian Gookool <72839719+amgookool@users.noreply.github.com>
2024-05-30 14:52:19 +0000

    Added JWT Middleware and removed un-needed middleware file

--- server/app.ts
+++ server/app.ts
@@ -4,12 +4,41 @@ import { logger } from "hono/logger";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { jwt } from "hono/jwt";
 import { csrf } from "hono/csrf";
-import {authenticationRoute,productsRoute, schedulesRoute, usersRoute} from "@server/api/index";
+import {
+  authenticationRoute,
+  productsRoute,
+  schedulesRoute,
+  usersRoute,
+} from "@server/api/index";
 
 const app = new Hono();
 app.use(csrf());
 app.use("*", logger());
 app.use(trimTrailingSlash());
+app.use(
+  "/api/users/*",
+  jwt({
+    secret: process.env.JWT_SECRET || "secret",
+    alg: "HS256",
+    cookie: "access_token",
+  })
+);
+app.use(
+  "/api/products/*",
+  jwt({
+    secret: process.env.JWT_SECRET || "secret",
+    alg: "HS256",
+    cookie: "access_token",
+  })
+);
+app.use(
+  "/api/schedules/*",
+  jwt({
+    secret: process.env.JWT_SECRET || "secret",
+    alg: "HS256",
+    cookie: "access_token",
+  })
+);
 
 const apiRoutes = app
   .basePath("/api")
@@ -18,8 +47,8 @@ const apiRoutes = app
   .route("/schedules", schedulesRoute)
   .route("/auth", authenticationRoute);
 
-app.get("*", serveStatic({root:"./frontend/dist"}))
-app.get("*",serveStatic({path:"./frontend/dist/index.html"}))
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
 export type ApiRoutes = typeof apiRoutes;
