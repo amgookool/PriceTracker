233e60aad84474fc206c36f0578a0b011d5c6fd1
Adrian Gookool <72839719+amgookool@users.noreply.github.com>
2024-05-30 14:46:45 +0000

    Updated App and Index

--- server/app.ts
+++ server/app.ts
@@ -4,19 +4,7 @@ import { logger } from "hono/logger";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { jwt } from "hono/jwt";
 import { csrf } from "hono/csrf";
-
-const migrateUser = async (
-  username: string,
-  email: string,
-  password: string
-) => {
-  try {
-    console.log("migrate user");
-  } catch (e) {
-    const error = e as Error;
-    console.error(`Error Occured migrating user:${error.message}`);
-  }
-};
+import {authenticationRoute,productsRoute, schedulesRoute, usersRoute} from "@server/api/index";
 
 const app = new Hono();
 app.use(csrf());
@@ -25,10 +13,10 @@ app.use(trimTrailingSlash());
 
 const apiRoutes = app
   .basePath("/api")
-  .route("/products")
-  .route("/users")
-  .route("/schedules")
-  .route("/auth");
+  .route("/products", productsRoute)
+  .route("/users", usersRoute)
+  .route("/schedules", schedulesRoute)
+  .route("/auth", authenticationRoute);
 
 app.get("*", serveStatic({root:"./frontend/dist"}))
 app.get("*",serveStatic({path:"./frontend/dist/index.html"}))
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,30 @@
-import app from './app';
+import app from "./app";
+import { UserService } from "./services";
+
+const username = process.env.ADMIN_USERNAME || "admin";
+const email = process.env.ADMIN_EMAIL || "amgookool@hotmail.com";
+const password = process.env.ADMIN_PASSWORD || "P@ssword123!";
+
+const migrateUser = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  try {
+    console.log("migrate user:", username);
+    await UserService.migrateUser(username, email, password);
+  } catch (e) {
+    const error = e as Error;
+    console.error(`Error Occured migrating user:${error.message}`);
+  }
+};
+
+await migrateUser(username, email, password);
 
 const server = Bun.serve({
-    port:process.env.PORT || 2500,
-    hostname: "0.0.0.0",
-    fetch: app.fetch
+  port: process.env.PORT || 2500,
+  hostname: "0.0.0.0",
+  fetch: app.fetch,
 });
 
-console.log(`Server running on port:${server.port}`)
+console.log(`Server running on port:${server.port}`);
