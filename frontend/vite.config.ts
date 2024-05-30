432a5ea3b4be49596877bad10993da72bb80909b
Adrian Gookool <72839719+amgookool@users.noreply.github.com>
2024-05-30 14:58:47 +0000

    Updated Vite Config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: `http://127.0.0.1:${process.env.PORT || 2500}`,
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias:{
+      "@": path.resolve(import.meta.dir, "./src"),
+      "@server": path.resolve(import.meta.dir, "../server"),
+      "@types": path.resolve(import.meta.dir, "../types"),
+    }
+  }
+});
