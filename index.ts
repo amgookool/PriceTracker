789736b14b1b3e6aab5c4f5e5b4519b1488940a9
Adrian Gookool <amgookool@hotmail.com>
2024-07-02 19:29:25 -0400

    updated index

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import app from "./server/app";
-import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-import { db } from "@server/database";
-import { performMigrations } from "./server/migrate";
+import app from './server/app';
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
+import { db } from '@server/database';
+import { performMigrations } from './server/migrate';
 
-await migrate(db, { migrationsFolder: "./.drizzle" });
+await migrate(db, { migrationsFolder: './.drizzle' });
 await performMigrations();
 
 const server = Bun.serve({
-  port: process.env.PORT || 2500,
-  hostname: "0.0.0.0",
-  fetch: app.fetch,
+	port: process.env.PORT ?? 2500,
+	hostname: '0.0.0.0',
+	fetch: app.fetch,
 });
 
 console.log(`Server running on port:${server.port}`);
